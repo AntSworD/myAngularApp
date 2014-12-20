d1fa653e4c97bf1fac3eee41308a4008083c534b
AntSworD <zhengjj.asd@gmail.com>
2014-12-20 18:32:34 +0800

    fixed some module missed in package.json, update chapter9.js

--- app/controllers/chapter9.js
+++ app/controllers/chapter9.js
@@ -1,14 +1,14 @@
 // ngDisabled
 exports.ngDisabled = function(req, res) {
-  res.render('chapter9/ngDisabled')
+  res.render('chapter9/ngDisabled');
 };
 
 // ngSelected
 exports.ngSelected = function(req, res) {
-  res.render('chapter9/ngSelected')
+  res.render('chapter9/ngSelected');
 };
 
 // ngHref
 exports.ngHref = function(req, res) {
-  res.render('chapter9/ngHref')
+  res.render('chapter9/ngHref');
 };
